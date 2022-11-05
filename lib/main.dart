2503ab3ba8357ec6bc26c2003fbccff6b44985c5
Guilherme Viana <guibvianaribeiro@gmail.com>
2022-11-05 04:29:49 -0300

    Inicio do repositório

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Exemplo Firebase',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
