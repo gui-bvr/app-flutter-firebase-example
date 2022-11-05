6b938478edc557f4d0e69e1d038c03b877dcb1de
Guilherme Viana <guibvianaribeiro@gmail.com>
2022-11-05 16:48:46 -0300

    Configurações do Firebase - read

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
-// ignore_for_file: prefer_const_constructors, await_only_futures
+// ignore_for_file: prefer_const_constructors, await_only_futures, must_be_immutable, prefer_const_constructors_in_immutables
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp;
-  runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: 'AIzaSyA_ARHRl1IZc5jBhfy25UPsu2NQ-OWWkRk',
+      appId: '1:836677915557:android:d8a1e4c4580107a1874e4c',
+      messagingSenderId: '836677915557',
+      projectId: 'exemplo-7b6c2',
+      storageBucket: 'exemplo-7b6c2.appspot.com',
+    ),
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -25,8 +33,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final CollectionReference _exemplo =
+      FirebaseFirestore.instance.collection('exemplo');
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +56,27 @@ class MyHomePage extends StatelessWidget {
           )
         ],
       ),
+      body: StreamBuilder(
+          stream: _exemplo.snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+            if (streamSnapshot.hasData) {
+              return ListView.builder(
+                itemCount: streamSnapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final DocumentSnapshot documentSnapshot =
+                      streamSnapshot.data!.docs[index];
+                  return ListTile(
+                    title: Text(
+                      documentSnapshot['L6SgaLJkSigiPkUpZ48l'],
+                    ),
+                  );
+                },
+              );
+            }
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }),
     );
   }
 }
